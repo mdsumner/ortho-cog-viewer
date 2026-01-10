6685095e97c7c6c1c70a53b4351dc465fb063d44
Michael Sumner <mdsumner@gmail.com>
2026-01-10 05:59:12 +0000

    multi res

--- src/MeshRenderer.ts
+++ src/MeshRenderer.ts
@@ -146,6 +146,18 @@ export class MeshRenderer {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   }
 
+  updateTexture(image: HTMLImageElement | HTMLCanvasElement): void {
+    const gl = this.gl;
+    
+    if (!this.texture) {
+      this.setTexture(image);
+      return;
+    }
+
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  }
+
   private createOrthoMatrix(
     left: number, right: number,
     bottom: number, top: number,
